5262ec52e1341fbe707a0c025f432166aeabd8f0
HolyPerch <Kittipat_t@cmu.ac.th>
2025-04-23 22:22:06 +0700

    fix time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  around_action :set_time_zone
+
+  def set_time_zone(&block)
+    Time.use_zone('Asia/Bangkok', &block)
+  end
 
   def after_sign_in_path_for(resource)
     dashboard_path 
